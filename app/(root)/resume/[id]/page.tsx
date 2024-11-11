abb498874c8f55d64db90516f822f567e90f94c4
24s-s.okou <s.okou@24s.com>
2024-11-11 17:42:45 +0100

    next15 params updates

--- app/(root)/resume/[id]/page.tsx
+++ app/(root)/resume/[id]/page.tsx
@@ -1,5 +1,8 @@
-const ResumePage = ({ params }: SearchParamProps) => {
-  return <div>Resume num {params?.id}</div>;
+import { use } from "react";
+
+const ResumePage = ({ params }: { params: Promise<{ id: string }> }) => {
+  const data = use(params);
+  return <div>Resume num {data?.id}</div>;
 };
 
 export default ResumePage;
